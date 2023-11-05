8cf28c673349e1bb67a45dd39668cbf99f01579f
Andrei Arkhipov <it.arkhipov@gmail.com>
2023-11-05 16:18:01 +0300

    remote password set

--- conftest.py
+++ conftest.py
@@ -1,12 +1,22 @@
 from selene import browser
 import pytest
+from init import settings
+import allure
+from allure_commons.types import AttachmentType
+
+
+def pytest_addoption(parser):
+    parser.addoption('--password', default='not set')
 
 
 @pytest.fixture
-def browser_config():
+def browser_config(request):
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
+    settings.password = request.config.getoption('--password')
+
     yield
 
+    allure.attach(settings.password, 'password', AttachmentType.TEXT, '.txt')
     browser.quit()
